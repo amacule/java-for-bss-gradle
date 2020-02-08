7e770e5d9ba3e71361edacdfc7e3f15f9c7c72a8
Artur Matculevich <artur.matculevich@t-systems.com>
2020-02-08 23:10:54 +0300

    v0.1

--- src/test/java/ru/tsystems/amatcule/qa/selenium/BaseSeleniumTest.java
+++ src/test/java/ru/tsystems/amatcule/qa/selenium/BaseSeleniumTest.java
@@ -1,4 +1,18 @@
 package ru.tsystems.amatcule.qa.selenium;
 
+import org.junit.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
 public class BaseSeleniumTest {
+
+    @Test
+    public void testEquals(){
+        int expVal = 6;
+        int actVal = 0;
+        int x = 2;
+        actVal = x + x * x;
+        assertEquals(expVal,actVal,"Не равно");
+    }
 }
